1507d5ea3df21aa498a02484ffac39966819a58a
sagya53 <sagya53@gmail.com>
2022-01-24 10:11:21 +0530

    Integer Data Type

---
+++ StandardDataType/integerDataType.py
@@ -0,0 +1,8 @@
+#The int data type:
+date = 15
+month = 1
+year = 2022
+print("Date =", date, "-", month, "-", year)
+print("type(date) =", type(date))
+print("type(month) =", type(month))
+print("type(year) =", type(year))
