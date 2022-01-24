e4ea386e71ee27db8fb7e1936d31f392890a569f
sagya53 <sagya53@gmail.com>
2022-01-24 10:07:58 +0530

    number Data Type

--- StandardDataType/numberDataType.py
+++ StandardDataType/numberDataType.py
@@ -7,3 +7,18 @@ no = 26
 print("no =", no)
 print("type(no) =", type(no))
 print("id(no) =", id(no))
+
+print()
+#Assigning same value/identifier:
+a = 30
+print("Value a =", a)
+print("Data type of variable a =", type(a))
+print("ID of a =", id(a))
+b = 30
+print("Value b =", b)
+print("Data type of variable b =", type(b))
+print("ID of b =", id(b))
+c = a
+print("Value c =", c)
+print("Data type of variable c =", type(c))
+print("ID of c =", id(c))
