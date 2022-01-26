b82627e6a5bad9682994111c9f4391e60870c3c2
sagya53 <sagya53@gmail.com>
2022-01-26 21:36:21 +0530

    String Data Type

---
+++ SequentialType/StringDataType/String..py
@@ -0,0 +1,16 @@
+# Singlem String
+s1 = "Ganesh"
+print("s1 = ",s1)
+print("s1 = ",s1,"Type of s1 = ",type(s1))
+print("ID = ",id(s1))
+s2 = s1
+print(s2)
+print("ID = ",id(s2))
+
+
+# Multiline String
+ms1 = """Hello All! this 
+is python Scripting language"""
+print("ms1 = ",ms1)
+print("Type of ms1 = ",type(ms1))
+print(id(ms1))
