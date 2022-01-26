67bd5c4e2afbc47c3fe2d2f672143587c028aa36
sagya53 <sagya53@gmail.com>
2022-01-26 21:21:18 +0530

    Boolean Data Type

---
+++ Boolean/bool.py
@@ -0,0 +1,9 @@
+b1 = False
+b2 = False
+print("b1 = ",b1)
+print("b2 = ",b2)
+print("b1 == b2",b1 == b2)
+print("Id b1 =",id(b1))
+print("Id b2 =",id(b2))
+print("Type b1 = ",type(b1))
+print("Type b2 = ",type(b2))
