21e80807744075e6f180316834d1e276c00f41b0
sagya53 <sagya53@gmail.com>
2022-01-26 19:57:29 +0530

    mathematical abs Data Type

---
+++ Mathematical function/abs.py
@@ -0,0 +1,11 @@
+# Syntex
+# abs(no)
+# Absolute value
+no = 20
+print("no = ",no)
+print("abs(no) = ",abs(no))
+no1 = -6
+print("no1 = ",no1)
+print("abs(no1) = ",abs(no1))
+no2 = 20.12
+print("abs(no2) = ",abs(no2))
