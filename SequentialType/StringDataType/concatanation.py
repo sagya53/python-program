e6a319272ffcbe0606bc49a2e1f7eff41faf8167
sagya53 <sagya53@gmail.com>
2022-02-04 12:00:26 +0530

    String accessing Concatanation function

---
+++ SequentialType/StringDataType/concatanation.py
@@ -0,0 +1,8 @@
+s1 = "Good"
+s2 = "Evening"
+s3 = s1 + s2
+print("s1 =", s1)
+print("s2 =", s2)
+print("s3 =", s3)
+print("s1 + s2 + s3 =", (s1 + s2 + s3))
+print("s1 + ' ' + s2 + ' ' + s3 =", (s1 + " " + s2 + " " + s3))
