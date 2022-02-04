0670b852c2951aeb8df75457357997b812d3e1e3
sagya53 <sagya53@gmail.com>
2022-02-04 11:54:58 +0530

    String accessing split function

---
+++ SequentialType/StringDataType/split.py
@@ -0,0 +1,7 @@
+"""The split() method"""
+s1 = "Sourabha Malve INTELLISE IT"
+print("s1 =" , s1)
+print("s1.split() =", s1.split())
+s2 = "Sourabha Malve, INTELLISE IT"
+print("s2 =" , s2)
+print("s2.split(',') =", s2.split(','))
